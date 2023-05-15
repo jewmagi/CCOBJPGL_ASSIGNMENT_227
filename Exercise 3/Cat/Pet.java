32c57f0f6a581e2be8f73aea73c744994fef5de9
Jewel Gonzales <122065593+jewmagi@users.noreply.github.com>
2023-05-15 15:31:56 +0800

    Add files via upload

---
+++ Exercise 3/Cat/Pet.java
@@ -0,0 +1,10 @@
+public class Pet implements Himalayan {
+   
+    @Override
+    public void meow() {
+        System.out.println();
+        System.out.println("My cute Himalayan is meowing");
+        System.out.println();
+    };
+    
+}
